f5641fbf3ea09bd6b18ac4d28b71355e87137edb
Mo Arvan <mo.arvan@gmail.com>
2022-05-13 13:11:01 -0500

    Add artifact

---
+++ artifact/scripts/run.sh
@@ -0,0 +1,1 @@
+python src/Data\ Generation/generate_features.py --path_to_matthew "Bible bitexts/book_of_matthew" --path_to_john "Bible bitexts/book_of_john" --download_dir ./results.csv --path_to_lang_df "Bible experimental vars/bible_all_features_LANGUAGE"
